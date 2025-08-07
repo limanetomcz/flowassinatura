d09621326cd9c46cae88b9545d895cbface22d5a
Joao Ricardo <joaoricardomoura99@gmail.com>
2025-08-06 21:31:04 -0300

    fix/FLOW-47 bloqueia rota register e cria fallback para redirecionar para o login

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return redirect()->route(Auth::check() ? 'home' : 'login');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::fallback(function () {
+    return redirect('/login');
+});
