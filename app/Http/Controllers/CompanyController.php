23c641f4c00b8c219dfce8e2bebb252099c4b2b5
Joao Ricardo <joaoricardomoura99@gmail.com>
2025-08-10 22:33:07 -0300

    feat/FLOW-33 cria CompanyController

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+
+    public function index()
+    {
+        $company = Company::all();
+        return response()->json($company);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'document' => 'required|string',
+            'contact_email' => 'required|string',
+            'contact_number' => 'required|string',
+        ]);
+
+        $company = Company::create($data);
+
+        return response()->json($company, 201);
+    }
+
+    public function show($id)
+    {
+        $company = Company::findOrFail($id);
+        return response()->json($company);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'document' => 'required|string',
+            'contact_email' => 'required|string',
+            'contact_number' => 'required|string',
+        ]);
+
+        $company = Company::findOrFail($id);
+        $company->update($data);
+
+        return response()->json($company);
+    }
+
+    public function destroy($id)
+    {
+        $company = Company::findOrFail($id);
+        $company->delete();
+
+        return response()->json(null, 204);
+    }
+}
