7645290496355693bcbf903d1724f605b02dff2d
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-08-11 18:39:00 -0300

    feature/FLOW-27: Exibir mensagens claras na tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,11 @@
 @extends('adminlte::auth.login')
 
 @section('auth_footer')
+    @if (session('status'))
+        <div class="alert alert-danger text-center" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
 
     @if (Route::has('password.request'))
         <a class="btn btn-link" href="{{ route('password.request') }}">
